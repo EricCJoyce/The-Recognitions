58236a18fef34285880287e0d46d27390be66ba4
EricCJoyce <50152689+EricCJoyce@users.noreply.github.com>
2021-11-24 18:31:26 -0500

    Update edit_mask_artifacts.py

--- Action_Recognition/3_Train_Network/edit_mask_artifacts.py
+++ Action_Recognition/3_Train_Network/edit_mask_artifacts.py
@@ -1,7 +1,9 @@
 from enactment import *
 import os
+import shutil
 import subprocess
 import sys
+import time
 
 def main():
 	params = get_command_line_params()								#  Collect parameters
@@ -16,18 +18,45 @@ def main():
 
 	e.load_parsed_objects()
 
-	fh = open(e.enactment_name + '.editlist', 'w')					#  Save edits to an edit list.
+	edit_list = {}													#  Save edits to an edit list.
+	editlist_filename = e.enactment_name + '.' + params['instance'] + '.editlist'
+
+	if os.path.exists(editlist_filename):							#  Resuming previous work?
+		fh = open(editlist_filename, 'r')
+		for line in fh.readlines():
+			if line[0] != '#':
+				arr = line.strip().split('\t')
+				edit_list[ arr[0] ] = arr[1]
+		fh.close()
 
 	detections = e.list_detections( (lambda detection: detection.instance_name == params['instance']) )
 	for detection in detections:
-		args = ['./edit_artifacts', '-mask', detection.mask_path, '-img', detection.parent_frame]
-		output = str(subprocess.getoutput(' '.join(args)))			#  Collect output.
-		fh.write(detection.mask_path + '\t' + output + '\n')
-		if params['verbose']:
-			print(detection.mask_path + '\t' + output)
+		if detection.mask_path not in edit_list:					#  Do not redo previous work.
+			args = ['./edit_artifacts', '-mask', detection.mask_path, '-img', detection.parent_frame]
+			output = str(subprocess.getoutput(' '.join(args)))		#  Collect output.
+
+			edit_list[detection.mask_path] = output					#  Save to table.
+			update_edit_file(editlist_filename, edit_list, params)	#  Write to file.
+
+			if params['verbose']:
+				print(detection.mask_path + '\t' + output)
+	return
+
+def update_edit_file(editlist_filename, edit_list, params):
+	if os.path.exists(editlist_filename):							#  Back up the existing copy.
+		shutil.copy(editlist_filename, editlist_filename + '.backup')
+
+	fh = open(editlist_filename, 'w')
+	fh.write('#  Edit list for enactment "' + params['enactment'] + '", instance "' + params['instance'] + '"\n')
+	fh.write('#  Created ' + time.strftime('%l:%M%p %Z on %b %d, %Y') + '\n')
+																	#  Yes, I made this a float because in cases when I had
+																	#  to manually split a mask, I made the daughter mask *.5.
+																	#  e.g. Enactment1/GT/mask_1.png
+																	#       Enactment1/GT/mask_1.5.png
+	for k, v in sorted(edit_list.items(), key=lambda x: float('.'.join(x[0].split('_')[-1].split('.')[:-1]))):
+		fh.write(k + '\t' + v + '\n')								#  Write to file.
 
 	fh.close()
-
 	return
 
 def get_command_line_params():
