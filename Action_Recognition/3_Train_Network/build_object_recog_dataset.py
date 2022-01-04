44b324a48271429bd677226bea2ceed90e5ec3b3
EricCJoyce <50152689+EricCJoyce@users.noreply.github.com>
2022-01-03 23:37:12 -0500

    Update build_object_recog_dataset.py

--- Action_Recognition/3_Train_Network/build_object_recog_dataset.py
+++ Action_Recognition/3_Train_Network/build_object_recog_dataset.py
@@ -55,25 +55,55 @@ def main():
 
 #  Iterate over all specified enactments and identify acceptable training samples.
 def build_dataset(classes, params):
-	object_lookup = {}												#  key:object-name ==> val:[images that contain an acceptable sample of this object]
+	object_lookup_images = {}										#  key: learnable-object-name ==> val: [list of images containing this learnable object]
+	image_lookup_objects = {}										#  key: image-file-name ==> val: [ (class-name, enactment, dimensions,
+																	#                                   maskpath, bboxstr, pixels-occupied), ... ]
+	learnable_instances = {}										#  key: class-name ==> val: the number of instances we wish to admit
+	counts = {}														#  key: class-name ==> val: number of instances actually collected
+	counts_train = {}
+	counts_valid = {}
 
-	image_lookup = {}												#  key:image-file ==> val:[ (image-file, class-name, enactment, dimensions,
-																	#                            maskpath, bboxstr), ... ]
-	targets = {}													#  key:class-name ==> val:the number of instances we wish to admit
+	include_conditions = {}											#  key: enactment name ==> val: [ (timestamp, timestamp), (timestamp, timestamp), ... ]
+	exclude_conditions = {}											#  key: enactment name ==> val: [ (timestamp, timestamp), (timestamp, timestamp), ... ]
+	if params['conditions-file'] is not None:
+		fh = open(params['conditions-file'], 'r')
+		for line in fh.readlines():
+			if line[0] != '#':
+				arr = line.strip().split('\t')
+				enactment = arr[0]
+				command = arr[1]
 
-	train = []
-	valid = []
+				times = []
+				i = 2
+				while i < len(arr):
+					time_a = float(arr[i])
+					time_b = float(arr[i + 1])
+					times.append( (time_a, time_b) )
+					i += 2
 
-	for enactment in params['enactments']:							#  Survey enactments directories
+				if command == 'INCLUDE':
+					include_conditions[enactment] = times[:]
+				elif command == 'EXCLUDE':
+					exclude_conditions[enactment] = times[:]
+		fh.close()
+
+	for enactment in params['enactments']:							#  Survey enactments directories.
 		if params['verbose']:
-			print('>>> Scanning ' + enactment + ' for all instances of learnable objects...')
+			if enactment in include_conditions:
+				print('>>> Scanning ' + enactment + ' for all instances of learnable objects within given intervals...')
+			elif enactment in exclude_conditions:
+				print('>>> Scanning ' + enactment + ' for all instances of learnable objects, excluding given intervals...')
+			else:
+				print('>>> Scanning ' + enactment + ' for all instances of learnable objects...')
 
 		fh = open(enactment + '_props.txt', 'r')
 		lines = fh.readlines()
 		for line in lines:
-			if line[0] != '#':										#  timestamp
-				arr = line.strip().split('\t')						#  image-filename
-				imgfilename = arr[1]								#  instance
+			if line[0] != '#':
+				arr = line.strip().split('\t')
+				timestamp = float(arr[0])							#  timestamp
+				imgfilename = arr[1]								#  image-filename
+																	#  instance
 				classpresent = arr[3]								#  class
 																	#  detection-source
 																	#  confidence
@@ -81,67 +111,132 @@ def build_dataset(classes, params):
 				maskpath = arr[7]									#  mask-filename
 																	#  3D-centroid-Avg
 																	#  3D-centroid-BBox
-				if classpresent in classes:							#  Here is an instance of one of the things we want to learn.
+				p = False
+				if enactment in include_conditions:					#  Scan inclusion intervals. If timestamp fits, accept.
+					i = 0
+					while i < len(include_conditions[enactment]):
+						if timestamp >= include_conditions[enactment][i][0] and timestamp < include_conditions[enactment][i][1]:
+							p = True
+							break
+						i += 1
+				elif enactment in exclude_conditions:				#  Scan exclusion intervals. If timestamp does NOT fit, accept.
+					i = 0
+					while i < len(exclude_conditions[enactment]):
+						if timestamp >= exclude_conditions[enactment][i][0] and timestamp < exclude_conditions[enactment][i][1]:
+							break
+						i += 1
+					if i == len(exclude_conditions[enactment]):
+						p = True
+				else:												#  No conditions apply: accept.
+					p = True
+
+				if p:
 					mask = cv2.imread(maskpath, cv2.IMREAD_UNCHANGED)
 					dimensions = str(mask.shape[1]) + ',' + str(mask.shape[0])
 					indices = np.where(mask == 255)
+					pixels_occupied = len(indices[0])
 
-					if len(indices[0]) >= params['minpx']:			#  Is it large enough to be helpful?
-						if imgfilename not in image_lookup:
-							image_lookup[imgfilename] = []
-						image_lookup[imgfilename].append( (imgfilename, classpresent, enactment, dimensions, maskpath, bbox_str) )
+					if imgfilename not in image_lookup_objects:
+						image_lookup_objects[imgfilename] = []
 
-						if classpresent not in object_lookup:
-							object_lookup[classpresent] = []
-																	#  Make sure there is only one instance of any file name.
-						if imgfilename not in object_lookup[classpresent]:
-							object_lookup[classpresent].append(imgfilename)
+					if classpresent not in params['ignore']:		#  As long as we're not outright ignoring this class, list it as a (collateral) candidate.
+						image_lookup_objects[imgfilename].append( (classpresent, enactment, dimensions, maskpath, bbox_str, pixels_occupied) )
+
+					if classpresent in classes:						#  Here is an instance of one of the things we want to learn.
+						if pixels_occupied >= params['minpx']:		#  Is it large enough to be helpful?
+							if classpresent not in object_lookup_images:
+								object_lookup_images[classpresent] = []
+							object_lookup_images[classpresent].append( imgfilename )
+
+							if classpresent not in learnable_instances:
+								learnable_instances[classpresent] = 0
+							learnable_instances[classpresent] += 1
+
 		fh.close()
 
-	for classname in classes:										#  Set defaults.
-		targets[classname] = len(object_lookup[classname])			#  By default, no limit on how many (acceptable) samples admitted.
-
-	if params['balance']:											#  Balance the sets: reset 'targets'.
+	if params['balance']:											#  Balance the sets: reset 'learnable_instances'.
 		least = float('inf')
-		for k, v in object_lookup.items():							#  Find the least-represented class.
+		for k, v in object_lookup_images.items():					#  Find the least-represented class.
 			if len(v) < least:
 				least = len(v)
-		for k in targets.keys():									#  Clamp all classes to that length.
-			targets[k] = least
+		for k in learnable_instances.keys():						#  Clamp all classes to that length.
+			learnable_instances[k] = least
 
-	if params['clamp'] < float('inf'):								#  Clamp: reset 'targets'.
-		for k in targets.keys():									#  Maximum is [:inf].
-			targets[k] = params['clamp'] + 1						#  Minimum is [:2].
+	if params['clamp'] < float('inf'):								#  Clamp: reset 'learnable_instances'.
+		for k in learnable_instances.keys():						#  Maximum is [:inf].
+			learnable_instances[k] = max(params['clamp'], 2)		#  Minimum is [:2].
 
-	used = {}														#  key:image-file ==> val:True
+	used = {}														#  key: image-file-name ==> val: True
+	train = []														#  List of tuples: (class-name, enactment, dimensions, maskpath, bboxstr)
+	valid = []														#  List of tuples: (class-name, enactment, dimensions, maskpath, bboxstr)
+
+	for classname in classes:
+		if classname not in counts:									#  Initialize actual counts.
+			counts[classname] = 0
+		if classname not in counts_train:
+			counts_train[classname] = 0
+		if classname not in counts_valid:
+			counts_valid[classname] = 0
 																	#  Start from the least populous class because it is the most fragile.
-	for classname, samples in sorted(object_lookup.items(), key=lambda x: len(x[1])):
-		samples = [x for x in samples if x not in used]				#  Filter out anything that has already been allocated to something else.
+	for classname, samples in sorted(object_lookup_images.items(), key=lambda x: len(x[1])):
+		filtered_samples = list(dict.fromkeys(samples))				#  Filter out duplicates.
+																	#  Filter out anything that has already been allocated to one of the sets.
+		filtered_samples = [x for x in filtered_samples if x not in used]
 
-		if len(samples) > 0:
+		if len(filtered_samples) > 0:
 																	#  Ensure we have at least one!
-			train_set_size = min( int(round(float(len(samples)) * params['train'])), targets[classname] - 1 )
+			train_set_size = min( int(round(float(len(filtered_samples)) * params['train'])), learnable_instances[classname] - 1 )
 
-			np.random.shuffle(samples)								#  Shuffle IN PLACE!
+			np.random.shuffle(filtered_samples)						#  Shuffle IN PLACE!
 
-			allocate_for_train = samples[:train_set_size]
-			allocate_for_valid = samples[train_set_size:]
+			allocate_for_train = filtered_samples[:train_set_size]	#  Image file names.
+			allocate_for_valid = filtered_samples[train_set_size:]
 
-			if params['verbose']:
-				print('      ' + str(len(samples)) + ' samples for ' + classname + ':\ttrain: ' + \
-				                 str(len(allocate_for_train)) + ',\tvalidate: ' + str(len(allocate_for_valid)))
+			for image in allocate_for_train:
+				if params['include-collateral']:
+					for learnable_instance in image_lookup_objects[image]:
+																	#  Omit pixels occupied.
+						train.append( tuple([image] + list(learnable_instance[:5])) )
+						counts[ learnable_instance[0] ] += 1
+						counts_train[ learnable_instance[0] ] += 1
+				else:
+					for learnable_instance in [x for x in image_lookup_objects[image] if x[5] >= params['minpx']]:
+																	#  Omit pixels occupied.
+						train.append( tuple([image] + list(learnable_instance[:5])) )
+						counts[ learnable_instance[0] ] += 1
+						counts_train[ learnable_instance[0] ] += 1
 
-			for imgname in allocate_for_train:
-				train += image_lookup[ imgname ]					#  Allocate these representative images to training set.
-				used[ imgname ] = True								#  Mark this image as used.
+				used[image] = True									#  Mark the image as used.
 
-			for imgname in allocate_for_valid:
-				valid += image_lookup[ imgname ]					#  Allocate these representative images to validation set.
-				used[ imgname ] = True								#  Mark this image as used.
-		else:
-			print('  WARNING!   No samples for ' + classname)
+			for image in allocate_for_valid:
+				if params['include-collateral']:
+					for learnable_instance in image_lookup_objects[image]:
+																	#  Omit pixels occupied.
+						valid.append( tuple([image] + list(learnable_instance[:5])) )
+						counts[ learnable_instance[0] ] += 1
+						counts_valid[ learnable_instance[0] ] += 1
+				else:
+					for learnable_instance in [x for x in image_lookup_objects[image] if x[5] >= params['minpx']]:
+																	#  Omit pixels occupied.
+						valid.append( tuple([image] + list(learnable_instance[:5])) )
+						counts[ learnable_instance[0] ] += 1
+						counts_valid[ learnable_instance[0] ] += 1
+
+				used[image] = True									#  Mark the image as used.
+
+	if params['verbose']:
+		for classname in classes:
+			if counts[ classname ] > 0:
+				print('      ' + str(counts[ classname ]) + ' samples for ' + classname + \
+				          ':\ttrain: ' + str(counts_train[ classname ]) + \
+				          ',\tvalidate: ' + str(counts_valid[ classname ]))
+			else:
+				print('  WARNING!   No samples for ' + classname)
+
+	dataset_timestamp = time.strftime('%l:%M%p %Z on %b %d, %Y')	#  Should be the same for training and validation set files.
 
 	fh = open('training-set.txt', 'w')								#  Write training set to file
+	fh.write('#  Training set for Object Recognition, created ' + dataset_timestamp + '\n')
 	fh.write('#  ' + ' '.join(sorted(classes)) + '\n')
 	fh.write('#  ' + ' '.join(sys.argv) + '\n')
 	fh.write('#  Image-file    Dimensions    Enactment    Learnable-object    B-Box    Mask-path\n')
@@ -156,6 +251,7 @@ def build_dataset(classes, params):
 	fh.close()
 
 	fh = open('validation-set.txt', 'w')							#  Write validation set to file
+	fh.write('#  Validation set for Object Recognition, created ' + dataset_timestamp + '\n')
 	fh.write('#  ' + ' '.join(sorted(classes)) + '\n')
 	fh.write('#  ' + ' '.join(sys.argv) + '\n')
 	fh.write('#  Image-file    Dimensions    Enactment    Learnable-object    B-Box    Mask-path\n')
@@ -179,6 +275,8 @@ def get_command_line_params():
 	params['valid'] = 0.2											#  Default
 	params['balance'] = False
 	params['clamp'] = float('inf')
+	params['include-collateral'] = False							#  Default
+	params['conditions-file'] = None								#  By default, use no conditions file.
 
 	params['minpx'] = 100											#  Default
 
@@ -194,7 +292,7 @@ def get_command_line_params():
 
 	argtarget = None												#  Current argument to be set
 																	#  Permissible setting flags
-	flags = ['-e', '-train', '-valid', '-b', '-c', '-minpx', '-x', '-colors', \
+	flags = ['-e', '-train', '-valid', '-b', '-c', '-minpx', '-coll', '-cond', '-x', '-colors', \
 	         '-imgw', '-imgh', '-User', \
 	         '-v', '-?', '-help', '--help']
 	for i in range(1, len(sys.argv)):
@@ -203,6 +301,8 @@ def get_command_line_params():
 				params['verbose'] = True
 			elif sys.argv[i] == '-b':
 				params['balance'] = True
+			elif sys.argv[i] == '-coll':
+				params['include-collateral'] = True
 			elif sys.argv[i] == '-?' or sys.argv[i] == '-help' or sys.argv[i] == '--help':
 				params['helpme'] = True
 			else:
@@ -223,6 +323,8 @@ def get_command_line_params():
 					params['minpx'] = max(0, int(argval))
 				elif argtarget == '-c':
 					params['clamp'] = max(1, int(argval))
+				elif argtarget == '-cond':
+					params['conditions-file'] = argval
 				elif argtarget == '-imgw':
 					params['imgw'] = int(argval)
 				elif argtarget == '-imgh':
@@ -263,9 +365,15 @@ def usage():
 	print('')
 	print('        -b      Balance the classes in the dataset. This means limiting all samples to the least represented class.')
 	print('        -c      Clamp the maximum number of samples per class to this integer >= 1. (Default is infinity = no clamp.)')
+	print('        -coll   Include collateral learnable objects. Default is False; passing this flag makes it True.')
+	print('                The minimum number of pixels determines what is a "learnable object." An image useful for training an')
+	print('                object of the requisite size might also include an object of inadequate size. If this parameter is set')
+	print('                to True, then such collateral instances will be included in the dataset despite their being too small.')
+	print('        -cond   Following argument is the path to a "conditions file" used to impose conditions on the use of')
+	print('                given enactments. By default, no such file is expected.')
 	print('')
-	print('        -minpx  All object instances admitted to the data set must occupy at least this many pixels. Default is 100.')
-	print('')
+	print('        -minpx  All object instances admitted to the data set must occupy at least this many pixels.')
+	print('                Default is 100, and pixels are counted in the actual object mask--not the bounding box area.')
 	print('        -train  In (0.0, 1.0). Portion of valid data to be allocated for training. Default is 0.8.')
 	print('        -valid  In (0.0, 1.0). Portion of valid data to be allocated for validation. Default is 0.2.')
 	print('')
