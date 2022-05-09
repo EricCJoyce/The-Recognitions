f4d1382434ce0c9c22dd7d50fd121bce642077a7
EricCJoyce <50152689+EricCJoyce@users.noreply.github.com>
2022-05-09 00:47:08 -0400

    Delete setup.py

--- Action_Recognition/setup.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup, Extension
-
-module = Extension("DTW", sources=["DTWmodule.c"])
-
-setup(name="DTW", \
-      version="1.0", \
-      description="Use dynamic time warping to align sequences", \
-      ext_modules=[module])
